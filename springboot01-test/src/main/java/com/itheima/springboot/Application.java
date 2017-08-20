3de07cb57b0f0533286147c44af5c3f7671bc815
lyj <liuyongju@163.com>
2017-08-20 19:04:52 +0800

    第一次提交

---
+++ springboot01-test/src/main/java/com/itheima/springboot/Application.java
@@ -0,0 +1,23 @@
+package com.itheima.springboot;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // 代表为SpringBoot应用的运行主类
+public class Application {
+	
+	public static void main(String[] args) {
+		
+		
+		//SpringApplication.run(Application.class,args);
+		
+		/** 运行SpringBoot应用 */
+		SpringApplication springApplication = 
+				new SpringApplication(Application.class);
+		/** 设置横幅模式(设置关闭) */
+		springApplication.setBannerMode(Banner.Mode.OFF);
+		/** 运行 */
+		springApplication.run(args);
+	}
+}
